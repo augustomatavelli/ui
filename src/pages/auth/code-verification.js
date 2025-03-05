ab1ed437020f19e808e56c654329e2292bad87cb
Augusto Cesar Matavelli <109761171+augustomatavelli@users.noreply.github.com>
2025-03-05 15:30:50 -0300

    feat: traduzir textos de verificação de código e redefinição de senha para português

--- src/pages/auth/code-verification.js
+++ src/pages/auth/code-verification.js
@@ -1,29 +1,28 @@
 // material-ui
-import { Grid, Stack, Typography } from '@mui/material';
+import { Grid, Stack, Typography } from "@mui/material";
 
 // project import
-import AuthWrapper from 'sections/auth/AuthWrapper';
-import AuthCodeVerification from 'sections/auth/auth-forms/AuthCodeVerification';
+import AuthWrapper from "sections/auth/AuthWrapper";
+import AuthCodeVerification from "sections/auth/auth-forms/AuthCodeVerification";
 
 // ================================|| CODE VERIFICATION ||================================ //
 
 const CodeVerification = () => (
-  <AuthWrapper>
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Stack spacing={1}>
-          <Typography variant="h3">Enter Verification Code</Typography>
-          <Typography color="secondary">We send you on mail.</Typography>
-        </Stack>
-      </Grid>
-      <Grid item xs={12}>
-        <Typography>We`ve send you code on jone. ****@company.com</Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <AuthCodeVerification />
-      </Grid>
-    </Grid>
-  </AuthWrapper>
+	<AuthWrapper>
+		<Grid container spacing={3}>
+			<Grid item xs={12}>
+				<Stack spacing={1}>
+					<Typography variant="h3">Verificação de dois fatores</Typography>
+				</Stack>
+			</Grid>
+			<Grid item xs={12}>
+				<Typography>Digite o código que foi enviado para o email ****@company.com</Typography>
+			</Grid>
+			<Grid item xs={12}>
+				<AuthCodeVerification />
+			</Grid>
+		</Grid>
+	</AuthWrapper>
 );
 
 export default CodeVerification;
