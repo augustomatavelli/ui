58167263ba591378bc6a6660c580792a9c98290a
Augusto Cesar Matavelli <109761171+augustomatavelli@users.noreply.github.com>
2025-03-11 23:11:48 -0300

    feat: adicionar novas mensagens de erro para validação de email e código 2FA

--- src/utils/errors-messages/errors-messages.js
+++ src/utils/errors-messages/errors-messages.js
@@ -4,4 +4,7 @@ export const ErrorMessages = {
 	expired_password: "Senha expirada",
 	user_password_invalid: "Usuário ou senha inválido",
 	invalid_password: "Senha inválida",
+	email_not_found: "Email não encontrado",
+	failed_sent_email: "Falha ao enviar o email",
+	invalid_2fa_code: "Código inválido",
 };
