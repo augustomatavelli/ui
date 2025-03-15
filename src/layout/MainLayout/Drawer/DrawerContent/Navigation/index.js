f5f6463128852df3cc443a8e7a53bcd4655cfe8e
Augusto Cesar Matavelli <109761171+augustomatavelli@users.noreply.github.com>
2025-03-15 20:05:09 -0300

    feat: integrar contexto de usuário para filtrar itens de menu com base no tipo de usuário

--- src/layout/MainLayout/Drawer/DrawerContent/Navigation/index.js
+++ src/layout/MainLayout/Drawer/DrawerContent/Navigation/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useContext, useEffect, useLayoutEffect, useState } from "react";
 
 // material-ui
 import { useTheme } from "@mui/material/styles";
@@ -12,12 +12,14 @@ import { HORIZONTAL_MAX_ITEM, MenuOrientation } from "config";
 // project import
 import NavGroup from "./NavGroup";
 import menuItem from "menu-items";
+import UserContext from "contexts/UserContext";
 
 // ==============================|| DRAWER CONTENT - NAVIGATION ||============================== //
 
 const Navigation = () => {
-	const theme = useTheme();
+	const { user } = useContext(UserContext);
 
+	const theme = useTheme();
 	const downLG = useMediaQuery(theme.breakpoints.down("lg"));
 
 	const { menuOrientation } = useConfig();
@@ -25,25 +27,30 @@ const Navigation = () => {
 	const [selectedItems, setSelectedItems] = useState("");
 	const [selectedLevel, setSelectedLevel] = useState(0);
 	const [menuItems, setMenuItems] = useState({ items: [] });
-	console.log(menuItem);
+
 	useEffect(() => {
 		handlerMenuItem();
 		// eslint-disable-next-line
-	}, []);
+	}, [user]);
 
-	let getMenu = Menu();
+	const getMenu = Menu();
+
 	const handlerMenuItem = () => {
-		const isFound = menuItem.items.some((element) => {
-			if (element.id === "group-dashboard") {
+		const filteredItems = menuItem.items.filter((item) => {
+			if (item.id === "group-dashboard") {
 				return true;
 			}
-			return false;
+			if (item.id === "group-admin" && user.type !== "A") {
+				return false;
+			}
+			return true;
 		});
 
-		if (getMenu?.id !== undefined && !isFound) {
-			menuItem.items.splice(0, 0, getMenu);
-			setMenuItems(menuItem);
+		if (getMenu?.id !== undefined && !filteredItems.some((item) => item.id === getMenu.id)) {
+			filteredItems.splice(0, 0, getMenu);
 		}
+
+		setMenuItems({ items: filteredItems });
 	};
 
 	useLayoutEffect(() => {
@@ -58,9 +65,7 @@ const Navigation = () => {
 	let remItems = [];
 	let lastItemId;
 
-	//  first it checks menu item is more than giving HORIZONTAL_MAX_ITEM after that get lastItemid by giving horizontal max
-	// item and it sets horizontal menu by giving horizontal max item lastly slice menuItem from array and set into remItems
-
+	// Verifica se o número de itens excede o limite horizontal e ajusta o menu
 	if (lastItem && lastItem < menuItems.items.length) {
 		lastItemId = menuItems.items[lastItem - 1].id;
 		lastItemIndex = lastItem - 1;
@@ -95,6 +100,7 @@ const Navigation = () => {
 				);
 		}
 	});
+
 	return (
 		<Box
 			sx={{
