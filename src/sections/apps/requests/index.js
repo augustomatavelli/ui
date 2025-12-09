26b8dadc8749196ec84d61621bc0f008373d69ae
Augusto Cesar Matavelli <109761171+augustomatavelli@users.noreply.github.com>
2025-12-08 22:47:27 -0300

    fix: corrigir a importação do locale do dayjs para evitar duplicação

--- src/sections/apps/requests/index.js
+++ src/sections/apps/requests/index.js
@@ -14,10 +14,10 @@ import ScheduleFormLanding from "./ScheduleFormLanding";
 import ProductsContext from "contexts/ProductsContext";
 import OperationsContext from "contexts/OperationContext";
 import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
 import utc from "dayjs/plugin/utc";
 dayjs.extend(utc);
 dayjs.locale("pt-br");
-import "dayjs/locale/pt-br";
 
 const CreateRequestStepper = ({ aircraft }) => {
 	const { createRequest } = useRequest();
