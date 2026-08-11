d388fd3a10d902a0f73767f94fe1390efda648b5
Augusto Cesar Matavelli <109761171+augustomatavelli@users.noreply.github.com>
2026-08-11 10:03:15 -0300

    feat: adicionar mensagem de erro para matrícula não encontrada no RAB da ANAC

--- src/utils/errors-messages/errors-messages.js
+++ src/utils/errors-messages/errors-messages.js
@@ -23,6 +23,7 @@ export const ErrorMessages = {
 	request_landing_site_exists: "Existe uma solicitação em aberto com esse helicentro",
 	request_operation_exists: "Existe uma solicitação em aberto com esse serviço",
 	failed_to_search_registration: "Erro ao buscar os dados. Verifique a matrícula e tente novamente",
+	registration_not_found: "Matrícula não encontrada no RAB da ANAC",
 	orders_open: "Você precisa concluir todas as ordens de serviço antes de finalizar a solicitação",
 	orders_already_started: "Você não pode cancelar a solicitação, pois existe um serviço em execução ou já finalizado",
 	request_order_execution_or_finalized: "Você não pode alterar a solicitação, pois existe uma ordem em execução ou finalizada",
